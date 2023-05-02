f8ba6f50c5131e887237efa8a2ef6b53fa4d1397
Пидор Не <strangeman21@outlook.com>
2023-05-02 13:26:58 +0500

    Добавьте файлы проекта.

---
+++ BankLibrary/AccountStateHandler.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankLibrary
+{
+    public delegate void AccountStateHandler(object sender, AccountEventArgs e);
+
+    public class AccountEventArgs
+    {
+        private string _message = "Empty";
+        private decimal _sum = 0;
+
+        public string Message
+        {
+            get;
+            set; 
+        }
+
+        public decimal Sum
+        {
+            get 
+            { 
+                return _sum; 
+            }
+            private set => _sum = value;
+        }
+
+
+        public AccountEventArgs(string message, decimal sum)
+        {
+            Sum = sum;
+            Message = message;
+        }
+    }
+}
